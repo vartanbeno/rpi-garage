cfe846c0cc460bc2085f502a5d4342f61ce4a4f5
Vartan Benohanian <vartanbeno@gmail.com>
2019-06-07 01:21:54 -0400

    Set up simple Flask server
    Will be used to send HTTP requests to endpoints containing logic that
    controls the Raspberry Pi's GPIO pins.
    
    Signed-off-by: Vartan Benohanian <vartanbeno@gmail.com>

---
+++ server.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+import os
+
+from dotenv import load_dotenv
+from flask import Flask, request
+
+load_dotenv(verbose=True)
+
+app = Flask(__name__)
+port = os.getenv('PORT')
+password = os.getenv('PASSWORD')
+
+
+@app.route('/', methods={'POST'})
+def auth():
+    data = request.get_json()
+    if 'password' in data and data['password'] == password:
+        return 'OK', 200
+    else:
+        return 'Unauthorized', 401
+
+
+app.run(host='0.0.0.0', port=port)
