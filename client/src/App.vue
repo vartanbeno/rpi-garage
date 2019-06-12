559c0002377ed165ec75ea687cb9cf6efc6a1a52
Vartan Benohanian <vartanbeno@gmail.com>
2019-06-11 23:17:48 -0400

    wip: working on the form that will send request
    Signed-off-by: Vartan Benohanian <vartanbeno@gmail.com>

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,28 +1,41 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <FormBox/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import FormBox from './components/FormBox';
 
 export default {
     name: 'app',
     components: {
-        HelloWorld
+        FormBox
     }
 };
 </script>
 
 <style>
+* {
+    box-sizing: border-box;
+}
+
+html,
+body {
+    height: 100%;
+    margin: 0;
+    padding: 0;
+}
+
 #app {
     font-family: 'Avenir', Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
-    text-align: center;
     color: #2c3e50;
-    margin-top: 60px;
+    display: flex;
+    flex-direction: column;
+    min-height: 100%;
+    /* todo remove this bg color */
+    background-color: #555;
 }
 </style>
---
+++ client/src/components/FormBox.vue
@@ -0,0 +1,64 @@
+<template>
+  <form class="form-container">
+    <input class="password-input" type="password" v-model="password" placeholder="Enter Password">
+    <button class="garage-button" type="submit">Toggle Garage</button>
+  </form>
+</template>
+
+<script>
+export default {
+    name: 'FormBox',
+    data: () => ({
+        password: ''
+    })
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.form-container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+
+.form-container .password-input {
+    height: 30px;
+    width: 200px;
+    border-top: none;
+    border-left: none;
+    border-right: none;
+    font-size: 16px;
+}
+
+.form-container .password-input:focus {
+    outline: none;
+    border-bottom-color: #555;
+}
+
+.form-container .garage-button {
+    margin: 1rem;
+    padding: 0.3rem 1rem;
+    line-height: 1.7rem;
+    font-size: 16px;
+    font-weight: bold;
+    color: #fff;
+    border-radius: 5px;
+    outline: none;
+    cursor: pointer;
+    background-color: #42b883;      /* Vue green */
+    border: 2px solid #fff;
+}
+
+.form-container .garage-button:hover {
+    background-color: #39aa77;
+    font-size: 18px;
+    transition: all .2s;
+}
+
+.form-container .garage-button:active {
+    background-color: #2fa06d;
+    font-size: 18px;
+    transition: all .2s;
+}
+</style>
